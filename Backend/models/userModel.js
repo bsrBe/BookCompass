516798491eeae40c914194a0dec3102ad3336dd1
Michael <119756864+MichaelzewdieS@users.noreply.github.com>
2025-06-02 18:38:17 -0700

    Update userModel.js for blocking a user

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -46,7 +46,11 @@ const userSchema = new mongoose.Schema({
             required: true, // Field is optional
             default: "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp",
           },
-        isEmailConfirmed: { type: Boolean, default: false }, 
+        isEmailConfirmed: { type: Boolean, default: false },
+    isBlocked: {
+        type: Boolean,
+        default: false
+    },
         confirmationToken: String, 
         confirmationSentAt: Date,
 
